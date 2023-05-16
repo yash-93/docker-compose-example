818607d9793eb46213b3f057f155800bec8a4258
Yashdeep Bachhas <yashdeepbachhas93@gmail.com>
2023-05-17 01:50:18 +0530

    added cors

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 
 const routes = require('./routes');
 
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT;
+
+app.use(cors());
 
 app.use('/', routes);
 
